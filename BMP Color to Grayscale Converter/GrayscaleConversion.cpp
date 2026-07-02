44c74b04029c46bcb85ad2725a548ae64efd5de8
John Harris <johnelmerharris@gmail.com>
2026-07-02 16:08:49 -0400

    Success with OpenCV method

--- BMP Color to Grayscale Converter/GrayscaleConversion.cpp
+++ BMP Color to Grayscale Converter/GrayscaleConversion.cpp
@@ -17,7 +17,7 @@ bool GrayscaleConverter::ReadFile(std::string fullSourcePath, std::string destPa
 	strDestFileName.insert(strDestFileName.find("."), "_Grayscale");
 	strFullDestPath = strDestPath;
 	strFullDestPath.append(strDestFileName);
-	bmp = cv::imread(strFullSourcePath);
+	bmp = cv::imread(strFullSourcePath, cv::COLOR_RGB2BGR);
 	return false;
 }
 
@@ -26,15 +26,19 @@ bool GrayscaleConverter::Convert()
 	if (bmp.empty())
 		return false;
 
-	for (int r = 0; r < bmp.rows; r++)
-	{
-		for (int c = 0; c < bmp.cols; c++)
-		{
-			ProcessPixelWithLuminosityMethod(bmp.at<Pixel>(r, c));
-		}
-	}
+	//for (int r = 0; r < bmp.rows; r++)
+	//{
+	//	for (int c = 0; c < bmp.cols; c++)
+	//	{
+	//		ProcessPixelWithLuminosityMethod(bmp.at<Pixel>(r, c));
+	//	}
+	//}
 
-	cv::imwrite(strFullDestPath, bmp);
+	//cv::imwrite(strFullDestPath, bmp);
+
+	cv::Mat newBmp;
+	cv::cvtColor(bmp, newBmp, cv::COLOR_BGR2GRAY);
+	cv::imwrite(strFullDestPath, newBmp);
 
 	return true;
 }
@@ -42,12 +46,18 @@ bool GrayscaleConverter::Convert()
 /*
 Luminosity method without gamma correction
 0.2126 × R + 0.7152 × G + 0.0722 × B
+
+0.299 x R + 0.587 x G + 0.114 x B
 */
 void GrayscaleConverter::ProcessPixelWithLuminosityMethod(cv::Vec3b &bgrPixel)
 {
-    bgrPixel[0] *= 0.0722;
-    bgrPixel[1] *= 0.7152;
-    bgrPixel[2] *= 0.2126;
+    //bgrPixel[0] *= 0.0722;
+    //bgrPixel[1] *= 0.7152;
+    //bgrPixel[2] *= 0.2126;
+
+	bgrPixel[0] *= 0.114;
+	bgrPixel[1] *= 0.587;
+	bgrPixel[2] *= 0.299;
 }
 
 
